5b063bbba99632ae95661c8f0f3ad6d36a066da4
jki12 <sjak0227@naver.com>
2024-04-19 11:15:22 +0900

    feat: null filter 추가
    thread safe하도록 싱글턴으로 구현

---
+++ src/main/java/com/nhnacademy/aiotone/node/filter/impl/NullFilter.java
@@ -0,0 +1,25 @@
+package com.nhnacademy.aiotone.node.filter.impl;
+
+import com.nhnacademy.aiotone.measurement.BaseMeasurement;
+import com.nhnacademy.aiotone.node.filter.MeasurementFilter;
+import lombok.Getter;
+
+/**
+ * null이 들어오거나, time, value의 값이 null일 경우 false를 반환합니다.
+ */
+public class NullFilter implements MeasurementFilter {
+    @Getter
+    private static NullFilter instance;
+
+    private NullFilter() {
+    }
+
+    @Override
+    public boolean isPassed(BaseMeasurement measurement) {
+        return (measurement != null && measurement.getTime() != null && measurement.getValue() != null);
+    }
+
+    static {
+        instance = new NullFilter();
+    }
+}
