be606bd4c203bdd6ef21b4355f21763260085ac1
jki12 <sjak0227@naver.com>
2024-04-21 19:26:46 +0900

    fix: properties field로 가지고 있도록 수정

--- src/main/java/com/nhnacademy/aiotone/node/mqtt/MqttSubscriber.java
+++ src/main/java/com/nhnacademy/aiotone/node/mqtt/MqttSubscriber.java
@@ -4,18 +4,21 @@ import com.nhnacademy.aiotone.measurement.BaseMeasurement;
 import com.nhnacademy.aiotone.node.BaseNode;
 import com.nhnacademy.aiotone.properties.MqttSubscriberProperties;
 import com.nhnacademy.aiotone.wire.Wire;
+import lombok.Getter;
 import org.eclipse.paho.client.mqttv3.*;
 
 import java.util.LinkedList;
 import java.util.List;
 
 
+@Getter
 public class MqttSubscriber extends BaseNode {
     private final List<Wire<BaseMeasurement>> list = new LinkedList<>();
-
+    private final MqttSubscriberProperties properties;
     private final IMqttClient subscriber;
 
     public MqttSubscriber(MqttSubscriberProperties properties) throws MqttException {
+        this.properties = properties;
         subscriber = new MqttClient(properties.getUrl(), "mqttSubscriber");
 
         subscriber.setCallback(new DefaultMqttSubscriberCallback(list));
