3736a1159f233b7967ee8fc9013fe57eb3bda920
jki12 <sjak0227@naver.com>
2024-04-21 19:25:46 +0900

    fix: write option 추가, 강제로 flush 하는 부분 삭제

--- src/main/java/com/nhnacademy/aiotone/node/mqtt/MqttPublisher.java
+++ src/main/java/com/nhnacademy/aiotone/node/mqtt/MqttPublisher.java
@@ -3,6 +3,7 @@ package com.nhnacademy.aiotone.node.mqtt;
 import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.InfluxDBClientFactory;
 import com.influxdb.client.WriteApi;
+import com.influxdb.client.WriteOptions;
 import com.influxdb.client.domain.WritePrecision;
 import com.nhnacademy.aiotone.measurement.BaseMeasurement;
 import com.nhnacademy.aiotone.node.RunnableNode;
@@ -14,16 +15,27 @@ import lombok.Getter;
 public class MqttPublisher extends RunnableNode {
     private final InfluxDBClient influxDBClient;
     private final WriteApi api;
+    private final MqttPublisherProperties properties;
 
     private Wire<BaseMeasurement> wire;
 
     public MqttPublisher(MqttPublisherProperties properties) {
+        this.properties = properties;
+
         influxDBClient = InfluxDBClientFactory.create(properties.getUrl(),
                 properties.getToken().toCharArray(),
                 properties.getOrg(),
                 properties.getBucket());
 
-        api = influxDBClient.makeWriteApi();
+        WriteOptions writeOptions = WriteOptions.builder()
+                .batchSize(5_000)
+                .flushInterval(1_000)
+                .bufferLimit(10_000)
+                .jitterInterval(1_000)
+                .retryInterval(5_000)
+                .build();
+
+        api = influxDBClient.makeWriteApi(writeOptions);
     }
 
     public void wireIn(Wire<BaseMeasurement> wire) {
@@ -35,7 +47,5 @@ public class MqttPublisher extends RunnableNode {
         if (!wire.isEmpty()) {
             api.writeMeasurement(WritePrecision.S, wire.poll());
         }
-
-        api.flush();
     }
 }
