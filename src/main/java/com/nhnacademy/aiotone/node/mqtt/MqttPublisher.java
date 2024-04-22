8d4f2d1f5208e360ec33f7accd419e93f5df3b99
jki12 <sjak0227@naver.com>
2024-04-22 15:14:32 +0900

    fix: thread safe하게 api 수정

--- src/main/java/com/nhnacademy/aiotone/node/mqtt/MqttPublisher.java
+++ src/main/java/com/nhnacademy/aiotone/node/mqtt/MqttPublisher.java
@@ -1,8 +1,7 @@
 package com.nhnacademy.aiotone.node.mqtt;
 
 import com.influxdb.client.InfluxDBClient;
-import com.influxdb.client.WriteApi;
-import com.influxdb.client.WriteOptions;
+import com.influxdb.client.WriteApiBlocking;
 import com.influxdb.client.domain.WritePrecision;
 import com.nhnacademy.aiotone.measurement.BaseMeasurement;
 import com.nhnacademy.aiotone.node.BaseNode;
@@ -17,31 +16,20 @@ public class MqttPublisher extends BaseNode {
     private static final int THREAD_COUNT = 3;
 
     private final InfluxDBClient influxDBClient;
-    private final WriteApi api;
+    private final WriteApiBlocking api;
     public final List<Thread> threadList = new ArrayList<>();
 
     private Wire<BaseMeasurement> wire;
 
     public MqttPublisher(InfluxDBClient influxDBClient) {
         this.influxDBClient = influxDBClient;
-
-        WriteOptions writeOptions = WriteOptions.builder()
-                .batchSize(5_000)
-                .flushInterval(1_000)
-                .bufferLimit(10_000)
-                .jitterInterval(1_000)
-                .retryInterval(5_000)
-                .build();
-
-        api = influxDBClient.makeWriteApi(writeOptions);
+        api = influxDBClient.getWriteApiBlocking();
 
         for (int i = 0; i < THREAD_COUNT; ++i) {
             Thread t = new Thread(() -> {
-
                 while (!Thread.currentThread().isInterrupted()) {
-
                     if (wire != null && !wire.isEmpty()) {
-                        api.writeMeasurement(WritePrecision.S, wire.poll());
+                        api.writeMeasurement(WritePrecision.MS, wire.poll());
                     }
                 }
             });
