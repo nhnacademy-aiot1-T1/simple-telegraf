ecb1eb657f20ebd683aefd692bb0c067e4c8a17a
jki12 <sjak0227@naver.com>
2024-04-30 17:30:15 +0900

    fix: spring으로 돌아가도록 변경

--- src/main/java/com/nhnacademy/aiotone/Main.java
+++ src/main/java/com/nhnacademy/aiotone/Main.java
@@ -1,8 +1,21 @@
 package com.nhnacademy.aiotone;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class);
     }
 }
+
+
+
+
+
+
+
