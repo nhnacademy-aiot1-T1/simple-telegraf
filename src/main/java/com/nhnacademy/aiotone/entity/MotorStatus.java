d98176894e2c5395363967697adb19e029d75400
jki12 <sjak0227@naver.com>
2024-06-04 18:03:10 +0900

    fix: motor status -> motor running log 변경, 및 motor id 추가 / motor name Transient 처리

--- src/main/java/com/nhnacademy/aiotone/entity/MotorStatus.java
+++ src/main/java/com/nhnacademy/aiotone/entity/MotorStatus.java
@@ -2,6 +2,7 @@ package com.nhnacademy.aiotone.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,18 +10,22 @@ import java.time.LocalDateTime;
 
 @Entity
 @Setter
-@Table(name = "motor_status")
+@Getter
+@Table(name = "motor_running_log")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MotorStatus {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Transient
     @JsonProperty("m")
-    @Column(name = "motor_name")
     private String motorName;
 
-    @Column(name = "is_on")
+    @Column(name = "motor_id")
+    private Long motorId;
+
+    @Column(columnDefinition = "TINYINT(1)", name = "is_on")
     private Boolean isOn;
 
     @Column(name = "created_at")
